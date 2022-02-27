7be4c3597e3c1d861fe3726e31564138a8047038
rldnrl <devpark1002@gmail.com>
2022-02-27 15:52:04 +0900

    call 메서드

---
+++ 07.call 메서드.js
@@ -0,0 +1,16 @@
+var func = function (a, b, c) {
+  console.log(this, a, b, c)
+}
+
+func(1, 2, 3)               // window 1 2 3
+func.call({ x: 1 }, 4, 5, 6)        // { x: 1 } 4 5 6
+
+var obj = {
+  a: 1,
+  method: function (x, y) {
+    console.log(this.a, x, y)
+  }
+}
+
+obj.method(2, 3)              // 1 2 3
+obj.method.call({ a: 4 }, 5, 6)    // 4 5 6
